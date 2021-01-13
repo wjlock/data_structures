8dba2de76344fe565511a9db1a3217a47733fc41
Will <will.locklier@gmail.com>
2021-01-13 14:30:12 -0500

    add linked list

---
+++ list.js
@@ -0,0 +1,13 @@
+const nodeOne = {
+  next: nodeTwo,
+  value: 1,
+};
+const nodeTwo = {
+  next: nodeThree,
+  value: 2,
+};
+const nodeThree = {
+  next: null,
+  value: 3,
+};
+const reverseLinkedList = (node) => {};
